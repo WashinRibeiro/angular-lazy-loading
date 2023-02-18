b053bbbe313cb9ece451fa5ab4d2f3e8d018de83
Washington Ribeiro <washinuchiha@gmail.com>
2023-02-17 21:00:56 -0300

    feat: create customers and orders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }, { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
